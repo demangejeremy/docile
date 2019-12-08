78c3dcdde71c2164353fabcf40ffb50b96d34864
Jérémy DEMANGE <j@lachainemedias.com>
2019-12-08 23:09:19 +0100

    Base script.

--- docile.py
+++ docile.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import click
 
 @click.command()
@@ -9,6 +8,6 @@ def hello(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
         click.echo('Hello %s!' % name)
-        
+
 if __name__ == '__main__':
     hello()
--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ setuptools.setup(
      long_description_content_type="text/markdown",
      url="https://github.com/demangejeremy/docile",
      packages=setuptools.find_packages(),
+     install_requires=[
+        'click'
+    ],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",

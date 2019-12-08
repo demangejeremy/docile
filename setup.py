0b595f714915856a141c11eaa9d2b93d60e860c1
Jérémy DEMANGE <j@lachainemedias.com>
2019-12-08 22:42:36 +0100

    First commit.

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='docupy',  
+     name='docile',  
      version='0.0.1',
-     scripts=['docupy'] ,
+     scripts=['docile'] ,
      author="Jérémy DEMANGE",
      author_email="j@lachainemedias.com",
      description="Your automated Python documentation.",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/demangejeremy/docupy",
+     url="https://github.com/demangejeremy/docile",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",

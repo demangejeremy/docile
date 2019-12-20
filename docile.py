57909818ef4a72e5ecabee53fc74229dc121bd0e
Jérémy DEMANGE <j@lachainemedias.com>
2019-12-20 10:39:43 +0100

    MAJ Docile

--- docile.py
+++ docile.py
@@ -4,6 +4,7 @@ import click
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
               help='The person to greet.')
+
 def hello(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='docile',  
-     version='0.0.1',
+     version='0.0.2',
      scripts=['docile'] ,
      author="Jérémy DEMANGE",
      author_email="j@lachainemedias.com",
